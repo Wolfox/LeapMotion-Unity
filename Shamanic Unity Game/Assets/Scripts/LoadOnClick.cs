80eb12d77fb1d17c3da8c616a4b787d08d6da9ea
Tiago Pinto <pinttiago@gmail.com>
2015-03-28 21:26:08 +0100

    init menus

---
+++ Shamanic Unity Game/Assets/Scripts/LoadOnClick.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadOnClick : MonoBehaviour {
+
+	public GameObject LoadingScene;
+
+	public void LoadGame(int level) {
+		LoadingScene.SetActive(true);
+		Application.LoadLevel(level);
+	}
+
+	public void LoadMenu(int level) {
+		Application.LoadLevel(level);
+	}
+
+	public void Quit() {
+		Application.Quit();
+	}
+}
